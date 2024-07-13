83fed56bda94f49ea8e69d6045d2ab0f8029076e
Muhammad Huzaifa <157727281+2008huzaifa@users.noreply.github.com>
2024-07-13 17:34:47 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
+import { getFirestore, collection, addDoc, doc, getDocs, deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyAs1Doh3H0AVh0xHUe3llAw8MlJgTcFr_o",
@@ -13,26 +13,59 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-let ol = document.getElementById('ul')
 
-var getbtn = document.getElementById('btn')
-getbtn.addEventListener('click', async () => {
-    let inp = document.getElementById('inp')
-    
-    const docRef = await addDoc(collection(db, "todos"), {
-        input: inp.value
+let btn = document.getElementById('btn')
+btn.addEventListener('click', async () => {
+getol.innerHTML = ''
+    let inp = document.getElementById('input')
+
+    const docRef = await addDoc(collection(db, "todo"), {
+        name: inp.value
+
     });
-    
-    ol.innerHTML = ''
-    inp.value = ''
-  
-    async function abc() {
-        const abcd = await getDocs(collection(db, "todos"))
+    // console.log("Document written with ID: ", docRef.id);
+    get()
 
-        abcd.forEach((items) => {
-          
-            ol.innerHTML += `<li>${items.data().input}</li>`
-        })
-    }
-    abc()
 })
+let getol = document.getElementById('ol')
+async function get() {
+
+    const q = collection(db, "todo")
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+        //   console.log(doc.id, " => ", doc.data());
+
+        getol.innerHTML += `<li style="display: flex;justify-content: space-between;"><span>${doc.data().name}</span><span>
+<button class="abc" onclick="delet('${doc.id}')">Delete</button>
+<button class ="abc" onclick="edit('${doc.id}')">Edit</button> 
+</span></li><br>`
+
+    });
+
+
+}
+
+window.delet = delet
+window.edit = edit
+get()
+async function edit(e) {
+getol.innerHTML = ''
+// console.log(e);
+    let update = prompt('Enter Updated Value')
+    const washingtonRef = doc(db, "todo", e);
+
+    await updateDoc(washingtonRef, {
+        name: update
+    });
+    get()
+// location.reload();
+}
+async function delet(e) {
+getol.innerHTML = ''
+
+    // console.log(e);
+    await deleteDoc(doc(db, "todo", e));
+    // location.reload;
+get()
+}
