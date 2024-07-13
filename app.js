5efddd6bc0c4277278f7fbd19e97964f715c4ada
Muhammad Huzaifa <157727281+2008huzaifa@users.noreply.github.com>
2024-07-13 23:08:50 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-app.js";
 import { getFirestore, collection, addDoc, doc, getDocs, deleteDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.3/firebase-firestore.js";
 
@@ -14,58 +15,112 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+
+let nametodo = document.getElementById('todo')
+let btna = document.getElementById('btna')
+let change = document.getElementById('changename')
+btna.addEventListener('click', () => {
+
+    change.innerHTML = `<h1>${nametodo.value}</h1>`
+    getol.innerHTML = ''
+    get()
+})
+
+
 let btn = document.getElementById('btn')
 btn.addEventListener('click', async () => {
-getol.innerHTML = ''
     let inp = document.getElementById('input')
+if(inp.value == ''){
+    Swal.fire({
+        title: "The Value ?",
+        icon: "question"
+      });
+    return
+}
+    getol.innerHTML = ''
 
-    const docRef = await addDoc(collection(db, "todo"), {
+    const docRef = await addDoc(collection(db, nametodo.value), {
         name: inp.value
 
     });
     // console.log("Document written with ID: ", docRef.id);
     get()
-
+    inp.value = ''
 })
 let getol = document.getElementById('ol')
 async function get() {
 
-    const q = collection(db, "todo")
+    const q = collection(db, nametodo.value)
 
     const querySnapshot = await getDocs(q);
     querySnapshot.forEach((doc) => {
         //   console.log(doc.id, " => ", doc.data());
 
-        getol.innerHTML += `<li style="display: flex;justify-content: space-between;"><span>${doc.data().name}</span><span>
-<button class="abc" onclick="delet('${doc.id}')">Delete</button>
-<button class ="abc" onclick="edit('${doc.id}')">Edit</button> 
-</span></li><br>`
+        getol.innerHTML += `<li style="display: flex;justify-content: space-between;margin-top:20px;margin-bottom:20px ;background-color:rgb(211, 200, 200) ;padding:10px;border-radius:7px"><span>${doc.data().name}</span><span>
+<button style="background-color: red; onclick="delet('${doc.id}')">Delete</button>
+<button style="background-color: blue; onclick="edit('${doc.data().name}','${doc.id}')">Edit</button> 
+</span></li>`
 
     });
 
 
 }
 
-window.delet = delet
-window.edit = edit
-get()
-async function edit(e) {
-getol.innerHTML = ''
-// console.log(e);
-    let update = prompt('Enter Updated Value')
-    const washingtonRef = doc(db, "todo", e);
+window.delet = delet;
+window.edit = edit;
+get();
+async function edit(e,a) {
 
-    await updateDoc(washingtonRef, {
-        name: update
+    const ipAPI = e
+    const inputValue = ipAPI;
+    const { value: ipAddress } = await Swal.fire({
+        title: "Enter your Updated Value",
+        input: "text",
+        inputLabel:false,
+        inputValue,
+        showCancelButton: true,
+        inputValidator: (value) => {
+            if (!value) {
+                return "You need to write something!";
+            }
+        }
     });
-    get()
-// location.reload();
-}
-async function delet(e) {
-getol.innerHTML = ''
+    if (ipAddress) {
+        Swal.fire(`Your Updated Value  is ${ipAddress}`);
+        
+        getol.innerHTML = ''
+        const washingtonRef = doc(db, nametodo.value,a);
+        
+            await updateDoc(washingtonRef, {
+                name: ipAddress
+            });
+            get()
+    }
 
-    // console.log(e);
-    await deleteDoc(doc(db, "todo", e));
-    // location.reload;
-get()
+}
+
+function delet(e) {
+
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then(async (result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: "Deleted!",
+                text: "Your work has been deleted.",
+                icon: "success"
+            });
+            getol.innerHTML = ''
+            await deleteDoc(doc(db, nametodo.value, e));
+            get()
+
+        }
+    })
+
 }
